47c48a91477061acd23fb083a0ae4874e664955a
damien <damien.venant@outlook.com>
2024-01-29 00:10:59 +0100

    Test

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ func main() {
 
 	defer os.Exit(0)
 
+	web.AddRoute(web.GET, "/", func(req web.Request) string {
+		fmt.Println("First callback")
+		return "First callback"
+	})
+	web.AddRoute(web.GET, "/test", func(req web.Request) string {
+		fmt.Println("Second Callback")
+		return "Second callback"
+	})
+
 	s, err := communication.InitSocket("0.0.0.0", 8090, communication.TCP4)
 
 	if err != nil {
@@ -33,8 +42,29 @@ func main() {
 		os.Exit(-1)
 	}
 
+	req, err := web.ParseString(content)
+	result := web.Router(req)
+	err = communication.SendMessage(con, result)
+
+	con.Close()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+/*Function to display a message*/
+func displayReq(request *web.Request) {
+	fmt.Println(request.HttpVersion)
+	fmt.Println(request.Verb)
+	fmt.Println(request.Path)
+
+	for key, val := range request.Header {
+		fmt.Printf("%s : %s\n", key, val)
 	}
 }
