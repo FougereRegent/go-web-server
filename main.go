74a3eba7c04c2fe583e0a119057f1f78b3f5e5cc
damien <damien.venant@outlook.com>
2024-02-01 14:54:13 +0100

    Ajout de la reponse

--- main.go
+++ main.go
@@ -3,68 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
-	"web-server/communication"
 	"web-server/web"
 )
 
 func main() {
 	fmt.Println("=== Web Server ===")
 
-	defer os.Exit(0)
-
-	web.AddRoute(web.GET, "/", func(req web.Request) string {
-		fmt.Println("First callback")
-		return "First callback"
+	router := web.CreateRouter("0.0.0.0", 8080)
+	router.AddRoute(web.GET, "/", func(req web.Request) string {
+		return "Hello World"
 	})
-	web.AddRoute(web.GET, "/test", func(req web.Request) string {
-		fmt.Println("Second Callback")
-		return "Second callback"
+	router.AddRoute(web.GET, "/version", func(req web.Request) string {
+		return "v1.0"
 	})
 
-	s, err := communication.InitSocket("0.0.0.0", 8090, communication.TCP4)
+	err := router.Run()
 
 	if err != nil {
-		fmt.Println(err)
 		os.Exit(-1)
 	}
 
-	con, err := s.Listen()
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
-
-	content, err := communication.RecvMessage(con)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
-
-	req, err := web.ParseString(content)
-	result := web.Router(req)
-	err = communication.SendMessage(con, result)
-
-	con.Close()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
-}
-
-/*Function to display a message*/
-func displayReq(request *web.Request) {
-	fmt.Println(request.HttpVersion)
-	fmt.Println(request.Verb)
-	fmt.Println(request.Path)
-
-	for key, val := range request.Header {
-		fmt.Printf("%s : %s\n", key, val)
-	}
+	os.Exit(0)
 }
--- web/router.go
+++ web/router.go
@@ -1,21 +1,74 @@
 package web
 
-import "fmt"
+import (
+	"fmt"
+	"web-server/communication"
+)
 
 type RouteDictionary map[string]func(req Request) string
 
-var _dic RouteDictionary = make(RouteDictionary)
+type DefaultRouter struct {
+	Port int
+	Addr string
+	_dic RouteDictionary
+}
 
-func AddRoute(verb Verb, path string, callback func(req Request) string) error {
+func (router *DefaultRouter) AddRoute(verb Verb, path string, callback func(req Request) string) error {
 	key := buildKey(verb, path)
-	_dic[key] = callback
+	router._dic[key] = callback
 	return nil
 }
 
-func Router(request *Request) string {
+func (router *DefaultRouter) router(request *Request) string {
 	key := buildKey(request.Verb, request.Path)
-	callbak := _dic[key]
-	return callbak(*request)
+	if callbak, ok := router._dic[key]; ok {
+		return callbak(*request)
+	}
+	return "Not Found"
+}
+
+func (router *DefaultRouter) Run() error {
+	socket, err := communication.InitSocket(router.Addr, uint(router.Port), communication.TCP4)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	for {
+		con, err := socket.Listen()
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		content, err := communication.RecvMessage(con)
+		if err != nil {
+			fmt.Println(err)
+			con.Close()
+			continue
+		}
+
+		req, err := ParseString(content)
+		if err != nil {
+			fmt.Println(err)
+			con.Close()
+			continue
+		}
+
+		result := router.router(req)
+		communication.SendMessage(con, result)
+		communication.CloseConnection(con)
+	}
+	return nil
+}
+
+func CreateRouter(addr string, port int) DefaultRouter {
+	return DefaultRouter{
+		Port: port,
+		Addr: addr,
+		_dic: make(RouteDictionary),
+	}
 }
 
 func buildKey(verb Verb, path string) string {
